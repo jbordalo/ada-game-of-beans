1e17e2b6bf8067514013a9b2790627aaafa2170c
Jacintinha <jlo.sousa@campus.fct.unl.pt>
2021-03-31 01:12:47 +0100

    First solution (needing heavy refactor)

--- src/GameOfBeans.java
+++ src/GameOfBeans.java
@@ -18,32 +18,60 @@ public class GameOfBeans {
 		}
 		return sum;
 	}
+
+	private int[] Pieton(int i, int f) {
+		int max = Integer.MIN_VALUE;
+		int[] indexes = new int [2];
+		for(int n = 0; n < this.gameDepth && i+n <= f; n++) {
+			int posChoice = score(i,i+n);
+			if(posChoice > max){
+				indexes[0] = i+n+1;
+				indexes[1] = f;
+				max = posChoice;
+			}
+			int negChoice = score(f-n, f);
+			if(negChoice > max){
+				indexes[0] = i;
+				indexes[1] = f-n-1;
+				max = negChoice;
+			}
+		}
+		if(indexes[1] - indexes[0] < 0) return null;
+		return indexes;
+	}
 	
 	public int computeScore() {
 
-		int[][] scores = new int[pile.length+1][pile.length+1];
-
-		for (int i = 0; i <= pile.length; i++) {
-			scores[i][i] = pile[i];
+		int[][] scores = new int[pile.length][pile.length];
+		// Fill first line with the pile values
+		for (int i = 0; i < pile.length; i++) {
+			scores[0][i] = pile[i];
 		}
 		
-		for (int i = 1; i <= pile.length; i++) {
-			for (int j = i; j <= pile.length; j++) {
-				
-				int max = -1;
-				// POSSIBLE MOVES
-				for(int n = 0; n < this.gameDepth; n++) {
-					int posChoice = score(i+n, j);
-					max = Math.max(max, posChoice + scores[i+n][j]);
-					
-					int negChoice = score(i, j-n);
-					max = Math.max(max, negChoice + scores[i][j-1]);
-					
+		for (int f = 1; f < pile.length; f++) {
+			for (int i = 0; i < pile.length-f; i++) {
+
+					int max = Integer.MIN_VALUE;
+					// POSSIBLE MOVES
+					for(int n = 0; n < this.gameDepth && n <= f; n++) {
+						int posChoice = score(i,i+n);
+						int s = 0;
+
+						int [] pietonPos = (f-n-1 < 1)? null : Pieton(i+n+1, f+i);
+						s =  (pietonPos == null) ? 0 : scores[pietonPos[1]-pietonPos[0]][pietonPos[0]];
+
+						max = Math.max(max, posChoice + s);
+						int negChoice = score(f-n+i, f+i);
+						s = 0;
+						int [] pietonNeg = (f-n-1 < 1)? null: Pieton(i, f+i-n-1);
+						s = (pietonNeg == null)? 0 : scores[pietonNeg[1]-pietonNeg[0]][pietonNeg[0]];
+
+						max = Math.max(max, negChoice + s);
+					}
+					scores[f][i] = max;
 				}
-				
-			}
 		}
 
-		return scores[pile.length][pile.length];
+		return scores[pile.length-1][0];
 	}
 }
