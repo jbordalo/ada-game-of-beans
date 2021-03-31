c65bb775ef18ac3fc176ea9512d962cf000cf0db
jbordalo <j.bordalo@campus.fct.unl.pt>
2021-03-31 01:36:54 +0100

    Add Pieton on first move
    Passing problem tests
    Runtime Error on Mooshak

--- src/GameOfBeans.java
+++ src/GameOfBeans.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class GameOfBeans {
 
@@ -21,57 +22,64 @@ public class GameOfBeans {
 
 	private int[] Pieton(int i, int f) {
 		int max = Integer.MIN_VALUE;
-		int[] indexes = new int [2];
-		for(int n = 0; n < this.gameDepth && i+n <= f; n++) {
-			int posChoice = score(i,i+n);
-			if(posChoice > max){
-				indexes[0] = i+n+1;
+		int[] indexes = new int[2];
+		for (int n = 0; n < this.gameDepth && i + n <= f; n++) {
+			int posChoice = score(i, i + n);
+			if (posChoice > max) {
+				indexes[0] = i + n + 1;
 				indexes[1] = f;
 				max = posChoice;
 			}
-			int negChoice = score(f-n, f);
-			if(negChoice > max){
+			int negChoice = score(f - n, f);
+			if (negChoice > max) {
 				indexes[0] = i;
-				indexes[1] = f-n-1;
+				indexes[1] = f - n - 1;
 				max = negChoice;
 			}
 		}
-		if(indexes[1] - indexes[0] < 0) return null;
+		if (indexes[1] - indexes[0] < 0)
+			return null;
 		return indexes;
 	}
-	
+
 	public int computeScore() {
+
+		// If Pieton moves first we need to find his move
+		if (!firstPlayer) {
+			int[] pietonChoice = Pieton(0, pile.length - 1);
+			this.pile = Arrays.copyOfRange(pile, pietonChoice[0], pietonChoice[1] + 1);
+		}
 
 		int[][] scores = new int[pile.length][pile.length];
 		// Fill first line with the pile values
 		for (int i = 0; i < pile.length; i++) {
 			scores[0][i] = pile[i];
 		}
-		
+
 		for (int f = 1; f < pile.length; f++) {
-			for (int i = 0; i < pile.length-f; i++) {
+			for (int i = 0; i < pile.length - f; i++) {
 
-					int max = Integer.MIN_VALUE;
-					// POSSIBLE MOVES
-					for(int n = 0; n < this.gameDepth && n <= f; n++) {
-						int posChoice = score(i,i+n);
-						int s = 0;
+				int max = Integer.MIN_VALUE;
+				// POSSIBLE MOVES
+				for (int n = 0; n < this.gameDepth && n <= f; n++) {
+					int posChoice = score(i, i + n);
+					int s = 0;
 
-						int [] pietonPos = (f-n-1 < 1)? null : Pieton(i+n+1, f+i);
-						s =  (pietonPos == null) ? 0 : scores[pietonPos[1]-pietonPos[0]][pietonPos[0]];
+					int[] pietonPos = (f - n - 1 < 1) ? null : Pieton(i + n + 1, f + i);
+					s = (pietonPos == null) ? 0 : scores[pietonPos[1] - pietonPos[0]][pietonPos[0]];
 
-						max = Math.max(max, posChoice + s);
-						int negChoice = score(f-n+i, f+i);
-						s = 0;
-						int [] pietonNeg = (f-n-1 < 1)? null: Pieton(i, f+i-n-1);
-						s = (pietonNeg == null)? 0 : scores[pietonNeg[1]-pietonNeg[0]][pietonNeg[0]];
+					max = Math.max(max, posChoice + s);
+					int negChoice = score(f - n + i, f + i);
+					s = 0;
+					int[] pietonNeg = (f - n - 1 < 1) ? null : Pieton(i, f + i - n - 1);
+					s = (pietonNeg == null) ? 0 : scores[pietonNeg[1] - pietonNeg[0]][pietonNeg[0]];
 
-						max = Math.max(max, negChoice + s);
-					}
-					scores[f][i] = max;
+					max = Math.max(max, negChoice + s);
 				}
+				scores[f][i] = max;
+			}
 		}
 
-		return scores[pile.length-1][0];
+		return scores[pile.length - 1][0];
 	}
 }
