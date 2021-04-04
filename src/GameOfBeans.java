64289346a867e027b3ca2aff8f4e22e7c146981a
jbordalo <j.bordalo@campus.fct.unl.pt>
2021-04-05 00:29:19 +0100

    Reformat code

--- src/GameOfBeans.java
+++ src/GameOfBeans.java
@@ -2,112 +2,110 @@ import java.util.Arrays;
 
 public class GameOfBeans {
 
-	private int[] pile;
-	private int gameDepth;
-	private boolean firstPlayer;
+    private int[] pile;
+    private int gameDepth;
+    private boolean firstPlayer;
 
-	public GameOfBeans(int[] pile, int gameDepth, boolean firstPlayer) {
-		this.pile = pile;
-		this.gameDepth = gameDepth;
-		this.firstPlayer = firstPlayer;
-	}
+    public GameOfBeans(int[] pile, int gameDepth, boolean firstPlayer) {
+        this.pile = pile;
+        this.gameDepth = gameDepth;
+        this.firstPlayer = firstPlayer;
+    }
 
-	private int score(int i, int j) {
-		int sum = 0;
-		for (int k = i; k <= j; k++) {
-			sum += pile[k];
-		}
-		return sum;
-	}
+    private int score(int i, int j) {
+        int sum = 0;
+        for (int k = i; k <= j; k++) {
+            sum += pile[k];
+        }
+        return sum;
+    }
 
-	private int[] Pieton(int i, int j) {
-		int max = Integer.MIN_VALUE;
-		int[] indexes = new int[2];
+    private int[] Pieton(int i, int j) {
+        int max = Integer.MIN_VALUE;
+        int[] indexes = new int[2];
 
-		// Left-side
-		for (int d = 0; d < this.gameDepth && i+d <= j; d++) {
-			int choice = score(i, i + d);
-			if (choice > max) {
-				indexes[0] = i + d + 1;
-				indexes[1] = j;
-				max = choice;
-			}
-		}
+        // Left-side
+        for (int d = 0; d < this.gameDepth && i + d <= j; d++) {
+            int choice = score(i, i + d);
+            if (choice > max) {
+                indexes[0] = i + d + 1;
+                indexes[1] = j;
+                max = choice;
+            }
+        }
 
-		// Right-side
-		for (int d = 0; d < this.gameDepth && i+d <= j; d++) {
-			int choice = score(j - d, j);
-			if (choice > max) {
-				indexes[0] = i;
-				indexes[1] = j - d - 1;
-				max = choice;
-			}
-		}
+        // Right-side
+        for (int d = 0; d < this.gameDepth && i + d <= j; d++) {
+            int choice = score(j - d, j);
+            if (choice > max) {
+                indexes[0] = i;
+                indexes[1] = j - d - 1;
+                max = choice;
+            }
+        }
 
-		if (indexes[1] - indexes[0] < 0)
-			return null;
+        if (indexes[1] - indexes[0] < 0)
+            return null;
 
-		return indexes;
-	}
+        return indexes;
+    }
 
-	public int computeScore() {
+    public int computeScore() {
+        // If Pieton moves first we need to find his move
+        if (!firstPlayer) {
+            int[] pietonChoice = Pieton(0, pile.length - 1);
+            if (pietonChoice == null) {
+                return 0;
+            }
+            // TODO
+            this.pile = Arrays.copyOfRange(pile, pietonChoice[0], pietonChoice[1] + 1);
+        }
 
-		// If Pieton moves first we need to find his move
-		if (!firstPlayer) {
-			int[] pietonChoice = Pieton(0, pile.length - 1);
-			if (pietonChoice == null) {
-				return 0;
-			}
-			// TODO
-			this.pile = Arrays.copyOfRange(pile, pietonChoice[0], pietonChoice[1] + 1);
-		}
+        int[][] scores = new int[pile.length][pile.length];
 
-		int[][] scores = new int[pile.length][pile.length];
+        // Fill first 'line' with the pile values
+        for (int i = 0; i < pile.length; i++) {
+            scores[i][i] = pile[i];
+        }
 
-		// Fill first 'line' with the pile values
-		for (int i = 0; i < pile.length; i++) {
-			scores[i][i] = pile[i];
-		}
+        int max;
+        int score;
 
-		int max;
-		int score;
+        for (int difference = 1; difference < pile.length; difference++) {
+            for (int i = 0; i < pile.length - difference; i++) {
+                int j = i + difference;
 
-		for (int difference = 1; difference < pile.length; difference++) {
-		    for (int i = 0; i < pile.length - difference; i++) {
-		        int j = i + difference;
+                max = Integer.MIN_VALUE;
 
-		        max = Integer.MIN_VALUE;
+                // Left-side
+                for (int d = 0; d < this.gameDepth && i + d <= j; d++) {
+                    score = score(i, i + d);
 
-		        // Left-side
-				for (int d = 0; d < this.gameDepth && i+d <= j; d++) {
-					score = score(i, i + d);
+                    int[] pietonPos = null;
+                    if (i + d != j) pietonPos = Pieton(i + d + 1, j);
 
-					int[] pietonPos = null;
-					if (i+d != j) pietonPos = Pieton(i + d + 1, j);
+                    score += (pietonPos == null) ? 0 : scores[pietonPos[0]][pietonPos[1]];
 
-					score += (pietonPos == null) ? 0 : scores[pietonPos[0]][pietonPos[1]];
+                    max = Math.max(max, score);
+                }
 
-					max = Math.max(max, score);
-				}
+                // Right-side
+                for (int d = 0; d < this.gameDepth && i + d <= j; d++) {
+                    score = score(j - d, j);
 
-				// Right-side
-				for (int d = 0; d < this.gameDepth && i+d <= j; d++) {
-					score = score(j-d, j);
+                    int[] pietonPos = null;
+                    if (i != j - d) pietonPos = Pieton(i, j - d - 1);
 
-					int[] pietonPos = null;
-					if (i != j-d) pietonPos = Pieton(i, j-d-1);
+                    score += (pietonPos == null) ? 0 : scores[pietonPos[0]][pietonPos[1]];
 
-					score += (pietonPos == null) ? 0 : scores[pietonPos[0]][pietonPos[1]];
+                    max = Math.max(max, score);
+                }
 
-					max = Math.max(max, score);
-				}
+                scores[i][j] = max;
 
-				scores[i][j] = max;
+            }
+        }
 
-		    }
-		}
-
-		return scores[0][pile.length-1];
-	}
-
+        return scores[0][pile.length - 1];
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,39 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-		int testCases = Integer.parseInt(input.readLine());
+        int testCases = Integer.parseInt(input.readLine());
 
-		for (int i = 0; i < testCases; i++) {
-			solveTestCase(input);
-		}
+        for (int i = 0; i < testCases; i++) {
+            solveTestCase(input);
+        }
 
-		input.close();
-	}
+        input.close();
+    }
 
-	public static void solveTestCase(BufferedReader input) throws IOException {
+    public static void solveTestCase(BufferedReader input) throws IOException {
 
-		String[] tokens = input.readLine().split(" ");
-		int nPiles = Integer.parseInt(tokens[0]);
-		int gameDepth = Integer.parseInt(tokens[1]);
+        String[] tokens = input.readLine().split(" ");
+        int nPiles = Integer.parseInt(tokens[0]);
+        int gameDepth = Integer.parseInt(tokens[1]);
 
-		String[] pileInput = input.readLine().split(" ");
-		int[] pile = new int[nPiles];
-		
-		for (int i = 0; i < pileInput.length; i++) {
-			pile[i] = Integer.parseInt(pileInput[i]);
-		}
-		
-		String firstPlayerInput = input.readLine();
-		boolean firstPlayer = firstPlayerInput.equals("Jaba");
+        String[] pileInput = input.readLine().split(" ");
+        int[] pile = new int[nPiles];
 
-		GameOfBeans game = new GameOfBeans(pile, gameDepth, firstPlayer);
+        for (int i = 0; i < pileInput.length; i++) {
+            pile[i] = Integer.parseInt(pileInput[i]);
+        }
 
-		int answer = game.computeScore();
+        String firstPlayerInput = input.readLine();
+        boolean firstPlayer = firstPlayerInput.equals("Jaba");
 
-		System.out.println(answer);
+        GameOfBeans game = new GameOfBeans(pile, gameDepth, firstPlayer);
 
-	}
+        int answer = game.computeScore();
+
+        System.out.println(answer);
+
+    }
 }
